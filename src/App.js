61380ccd1c79f22adc4e04173f53d469ad507469
mac <mac@macs-iMac.local>
2023-09-29 09:15:15 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import OrderDetails from "./components/OrderDetails/index";
 import DataGrid from "./components/DataGrid/index";
 import Navbar from "./components/Navbar";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 const App = () => {
-  const [selectedTransaction, setSelectedTransaction] = useState(null);
-  console.log(selectedTransaction);
-
   return (
     <Provider store={store}>
       <Navbar />
       <DataGrid />
-      <OrderDetails selectedTransaction={selectedTransaction} />
+      <OrderDetails />
     </Provider>
   );
 };
